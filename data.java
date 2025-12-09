36c1175871753276042bf5f4c4095a81077907f6
subathradhanavanthan <subathradhanavanthan@gmail.com>
2025-12-09 17:25:35 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,9 @@
+public class data{
+  public static void main(String arg[]){
+    System .out.println("java programming");
+    int num2=5;
+    int total= num2;
+    System.out.println("total,"+total);
+  }
+}
+    
