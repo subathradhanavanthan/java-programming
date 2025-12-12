3d36528919148a85db6a5c682fba999050de60d0
subathradhanavanthan <subathradhanavanthan@gmail.com>
2025-12-12 18:15:20 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class student{
+  public static void main(String arg[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a>=60){
+      System.out.println("the student is pass");
+  }
+    else{
+      System.out.println("the student is fail");
+  }
+  }
+}
+    
