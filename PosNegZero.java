be61d80ec0b099cef54903ac5fbe5aaddd06857f
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-01-27 18:14:58 +0530

    Add files via upload

---
+++ PosNegZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Grade{
+  public static void main (String[]args){
+    Scanner sc=new Scanner(System.in);
+      int m=Sc.nextInt();
+      if(m>=90)
+        system.out.println("A");
+      else if(m>=75)
+        system.out.println("B");
+      else if(m>=60)
+        system.out.println("C");
+      else if (m>=40)
+        system.out.println("D");
+      else
+        system.out.println("fail");
+  }
+}
