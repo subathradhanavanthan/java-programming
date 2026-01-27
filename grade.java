6d90078f087167c25c5c82e09573dc9cf4565f51
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-01-27 18:14:25 +0530

    Add files via upload

---
+++ grade.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Grade{
+  public static void main (String[]args){
+    Scanner sc=new Scanner(System.in);
+      int m=sc.nextInt();
+      if(m>=90)
+        System.out.println("A");
+      else if(m>=75)
+        System.out.println("B");
+      else if(m>=60)
+        System.out.println("C");
+      else if (m>=40)
+        System.out.println("D");
+      else
+        System.out.println("fail");
+  }
+}
