0198bf3b89c77260a85c57bab9c6a1edaf2a91c8
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-01-31 16:07:15 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class vote{
+  public static void main (String[]args){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age>=18){
+      System.out.println("eligible to vote");
+    }
+    else 
+      System.out.println("not eligible");
+  }
+}
