ee2e5af9e9010da32f1fb430f7d84a47c010f9b8
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-01-31 16:07:33 +0530

    Add files via upload

---
+++ PassFail.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class PassFail{
+  public static void main (String[]args){
+    Scanner sc=new Scanner(System.in);
+    int marks=sc. nextInt();
+    if(marks>=40){
+      System.out.println("pass");
+      }
+   else
+      System.out.println("fail");
+  }
+}
