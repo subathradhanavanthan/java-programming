ed39288db6412d2172753ff84e10c29b6bfb4e58
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-02-03 17:17:19 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Armstrong{
+  public static void main(String arg[]){
+    int n=153,temp=n,sum=0;
+    while(n>0){
+      int d=n%10;
+      sum+=d*d*d;
+      n/=10;
+    }
+    System.out.println(temp==sum);
+  }
+}
