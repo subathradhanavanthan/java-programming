955ef8a6e8a0a705b9978b70b127f4d3afcdafcd
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-02-09 17:24:43 +0530

    Add files via upload

---
+++ stopNumbers.java
@@ -0,0 +1,11 @@
+public class stopNumbers{
+  public static void main(String[]args){
+        for (int i = 1; i <= 20; i++) {
+            if (i == 15) {
+                break;
+            }
+            System.out.print(i);
+        }
+  }
+}
+      
