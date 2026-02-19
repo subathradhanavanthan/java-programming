c61a95611cbdf189bf0997dd81e3b216813a1519
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-02-19 17:58:18 +0530

    Add files via upload

---
+++ try.java
@@ -0,0 +1,14 @@
+class demo{
+  public static void main(String[]args){
+    try{
+      int a=10/0;
+       System.out.println(a);
+    } 
+    catch(ArithmeticException e){
+      System.out.println("cannot divide by zero");
+    }
+  finally{
+  System.out.println("program continues");
+  }
+  }
+}
